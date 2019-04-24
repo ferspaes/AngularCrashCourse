44c3b7ce7ebec17bae64bfcb10fa7e133648c667
fernando.js8 <fernando.js8@outlook.com>
2019-04-23 21:14:49 -0300

    resolving conflicts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,29 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NewWeightEntriesComponent } from './new-weight-entries/new-weight-entries.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NewWeightEntriesComponent,
+    LoginComponent,
   ],
   imports: [
     CommonModule,
     BrowserModule,
     FormsModule,
     HttpClientModule,
+    RouterModule.forRoot([
+      { path: '', redirectTo: '/home', pathMatch: 'full'},
+      { path: 'home', component: HomeComponent }
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
